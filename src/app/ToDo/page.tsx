72f3497f9f409518cefa6579f5f8d5145f8548b3
Naoki <gtvofromb@gmail.com>
2024-09-04 15:59:39 +0900

    commit

--- src/app/ToDo/page.tsx
+++ src/app/ToDo/page.tsx
@@ -1,17 +1,35 @@
 'use client'
 
 import Header from "../components/header";
-import { RefObject, useState } from "react";
+import React, { RefObject, useState } from "react";
 import { useRef } from "react";
+import internal from "stream";
+import { List } from "postcss/lib/list";
 
 const ToDo = () => {
 
     const inputValue: RefObject<HTMLInputElement> = useRef(null);
+    const [taskList, setTaskList] = useState<string[]>([])
 
     const handleClick = () => {
         if ( inputValue.current){
-            alert(inputValue.current.value)
+            var list: string[] = []
+            list = [...taskList]
+            list.push(inputValue.current.value)
+            setTaskList(list)
+            inputValue.current.value = ''
         }
+    }
+
+    const handleDelete = (index: number) => {
+        var list : string[] = []
+        list = [...taskList]
+        list.splice(index, 1)
+        setTaskList(list)
+    }
+
+    const handleEdit = (index: number) => {
+        
     }
 
     return (
@@ -19,6 +37,24 @@ const ToDo = () => {
             <Header />
             <input type='text' className="border" ref={inputValue}></input>
             <button onClick={handleClick}>追加する</button>
+            {taskList.map((task, index)=>{return(<ListDesign value={task} index={index} onDelClick={handleDelete} onEditClick={handleEdit}/>)})}
+        </div>
+    )
+}
+
+interface ListProps {
+    value: string;
+    index: number;
+    onDelClick(index: number): void;
+    onEditClick(index: number): void;
+}
+
+const ListDesign: React.FC<ListProps> = (props) => {
+    return (
+        <div>
+            {props.index}：{props.value}
+            <button className="border" onClick={() => props.onDelClick(props.index)}>削除</button>
+            <button className="border" onClick={() => props.onEditClick(props.index)}>編集</button>
         </div>
     )
 }
